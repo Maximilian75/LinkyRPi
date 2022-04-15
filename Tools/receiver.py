b802042ae8ab2e892244d01de1bbc2d01b14f58e
Maximilian75 <103195383+Maximilian75@users.noreply.github.com>
2022-04-15 08:57:13 +0200

    Update receiver.py

--- Tools/receiver.py
+++ Tools/receiver.py
@@ -1,6 +1,13 @@
 #!/usr/local/env python
 # -*- coding: utf-8 -*-
 
+#This file is part of LinkyRPi.
+#LinkyRPi is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#LinkyRPi is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License along with LinkyRPi. If not, see <https://www.gnu.org/licenses/>.
+#(c)Copyright Mikaël Masci 2022
+
+
 import posix_ipc
 import time
 import json
